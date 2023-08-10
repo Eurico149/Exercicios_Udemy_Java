6d7015600348b505f391e6be62f1b394a59dd829
eurico <euricogabriel149@gmail.com>
2023-08-09 21:19:32 -0300

    Exercicios Java Estrutura Sequencial

---
+++ EstrutuaSequencial/Exercicio_1.java
@@ -0,0 +1,11 @@
+package Exercicios_Udemy_Java.EstrutuaSequencial;
+import java.util.Scanner;
+
+public class Exercicio_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int numero1 = sc.nextInt(), numero2 = sc.nextInt();
+
+        System.out.println("SOMA = "+ (numero1 + numero2));
+    }
+}
