0446c14f4a2f1c0ca974cc31aceedb4d82a2da47
eurico <euricogabriel149@gmail.com>
2023-08-29 18:57:48 -0300

    Exercicios Java Estrutura Sequencial

--- EstrutuaSequencial/Exercicio_1.java
+++ EstrutuaSequencial/Exercicio_1.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Exercicio_1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int numero1 = sc.nextInt(), numero2 = sc.nextInt();
+        int numero1, numero2;
+        numero1 = sc.nextInt();
+        numero2 = sc.nextInt();
 
-        System.out.println("SOMA = "+ (numero1 + numero2));
+        System.out.print("SOMA = "+ (numero1 + numero2));
+        sc.close();
     }
 }
--- EstrutuaSequencial/Exercicio_2.java
+++ EstrutuaSequencial/Exercicio_2.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class Exercicio_2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        double raio = sc.nextDouble(), pi = 3.14159;
+        double raio = sc.nextDouble();
+        double pi = 3.14159;
         double resultado = pi * (raio * raio);
 
         System.out.printf("A=%.4f", resultado);
+        sc.close();
     }
 }
